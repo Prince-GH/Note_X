7f101dad2198b7e1e12343e48b877a7330955e26
Prince <150125481+Prince-GH@users.noreply.github.com>
2024-04-07 20:00:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ const editor = document.getElementById('editor');
 const count = document.getElementById('words');
 const data = document.getElementById('data');
 const mode = document.getElementById('mode');
-
+const done = document.getElementById('done');
+let text;
+window.onload = function(){
+    editor.value=localStorage.getItem("data");
+    count.innerHTML=localStorage.getItem("words");
+}
 editor.addEventListener("keyup", () => {
-    const text = editor.value;
+    text=editor.value;
+    localStorage.setItem("data",text);
     const wordCount = countWords(text);
+    localStorage.setItem("words",wordCount);
     console.log("Word count:", wordCount);
     count.innerHTML=wordCount;
 });
 
-function countWords(text) {
+function countWords(text){
     const words = text.trim().split(/\s+/);
     return words.length;
   }
-
 
 let click=1;
 mode.addEventListener('click',()=>{
@@ -36,4 +42,12 @@ mode.addEventListener('click',()=>{
         editor.style.color='black';
         mode.style.color='black';
     }
+});
+
+done.addEventListener('click',function (){
+navigator.clipboard.writeText(text);
+localStorage.removeItem("data");
+localStorage.removeItem("words");
+editor.value='';
+count.innerHTML='';
 });
